96781b540355d9dd5bbdc3c3be0da69db9f40c59
beomsik.park <parkbeomsik@kaist.ac.kr>
2022-11-09 16:15:32 +0900

    Pass value test for CNN with EW

--- examples/value_test/test.py
+++ examples/value_test/test.py
@@ -16,6 +16,23 @@ if not os.path.exists("value_data"):
 if not os.path.exists("grad_sample"):
     os.mkdir("grad_sample")
 
+def test(model_type, architecture, dpsgd_mode, grad_sample_mode):
+    print(f"Execute {dpsgd_mode} ({grad_sample_mode})...")
+    execute_command(f"python benchmark.py --input_size 32 --architecture {architecture} --model_type {model_type} --dpsgd_mode naive --batch_size 16 --model_load_path value_test/value_data/{architecture}_weight.pt --input_load_path value_test/value_data/{architecture}_input.pt --grad_save_path value_test/value_data/{architecture}_grad_{dpsgd_mode}_{grad_sample_mode}.pt --profile_value -c 0.1 --sigma 0.0 --grad_sample_mode ew")
+
+    ret = is_equal(f"value_test/value_data/{architecture}_grad_ref.pt",
+                   f"value_test/value_data/{architecture}_grad_{dpsgd_mode}_{grad_sample_mode}.pt",
+                    verbose=False)
+
+    if ret:
+        print(f"Results of {dpsgd_mode} ({grad_sample_mode}) is correct!")
+    else:
+        print(f"Results of {dpsgd_mode} ({grad_sample_mode}) is wrong!")
+        is_equal(f"value_test/value_data/{architecture}_grad_ref.pt",
+                 f"value_test/value_data/{architecture}_grad_{dpsgd_mode}_{grad_sample_mode}.pt",
+                 verbose=True)
+
+
 case_list = ["cnn resnet18", "cnn resnet50", "cnn resnet152"]
 
 for case in case_list:
@@ -28,36 +45,16 @@ for case in case_list:
 
     # Create reference grads (naive DPSGD)
     print("Execute naive...")
-    execute_command(f"python benchmark.py --input_size 32 --architecture {architecture} --model_type {model_type} --dpsgd_mode naive --batch_size 16 --model_load_path value_test/value_data/{architecture}_weight.pt --input_load_path value_test/value_data/{architecture}_input.pt --grad_save_path value_test/value_data/{architecture}_grad_naive.pt --profile_value -c 0.1 --sigma 0.0")
+    execute_command(f"python benchmark.py --input_size 32 --architecture {architecture} --model_type {model_type} --dpsgd_mode naive --batch_size 16 --model_load_path value_test/value_data/{architecture}_weight.pt --input_load_path value_test/value_data/{architecture}_input.pt --grad_save_path value_test/value_data/{architecture}_grad_ref.pt --profile_value -c 0.1 --sigma 0.0 --grad_sample_mode hooks")
+
+    # Test grads of DPSGD with ExpandedWeights
+    test(model_type, architecture, "naive", "ew")
 
     # Test grads of DPSGD(R)
-    print("Execute reweight...")
-    execute_command(f"python benchmark.py --input_size 32 --architecture {architecture} --model_type {model_type} --dpsgd_mode reweight --batch_size 16 --model_load_path value_test/value_data/{architecture}_weight.pt --input_load_path value_test/value_data/{architecture}_input.pt --grad_save_path value_test/value_data/{architecture}_grad_reweight.pt --profile_value -c 0.1 --sigma 0.0")
+    test(model_type, architecture, "reweight", "hooks")
 
-    reweight_ret = is_equal(f"value_test/value_data/{architecture}_grad_naive.pt",
-                            f"value_test/value_data/{architecture}_grad_reweight.pt",
-                            verbose=False)
+    # Test grads of DPSGD(R) with ExpandedWeights
+    test(model_type, architecture, "reweight", "ew")
 
-    if reweight_ret:
-        print("Results of reweight is correct!")
-    else:
-        print("Results of reweight is wrong!")
-        is_equal(f"value_test/value_data/{architecture}_grad_naive.pt",
-                 f"value_test/value_data/{architecture}_grad_reweight.pt",
-                 verbose=True)
-
-    # Test grads of elegant
-    print("Execute elegant ...")
-    execute_command(f"python benchmark.py --steps 1 --input_size 32 --architecture {architecture} --model_type {model_type} --dpsgd_mode elegant --batch_size 16 --model_load_path value_test/value_data/{architecture}_weight.pt --input_load_path value_test/value_data/{architecture}_input.pt --grad_save_path value_test/value_data/{architecture}_grad_elegant.pt --profile_value -c 0.1 --sigma 0.0")
-
-    elegant_ret = is_equal(f"value_test/value_data/{architecture}_grad_naive.pt",
-                           f"value_test/value_data/{architecture}_grad_elegant.pt",
-                           verbose=False)
-
-    if elegant_ret:
-        print("Results of elegant is correct!")
-    else:
-        print("Results of elegant is wrong!")
-        is_equal(f"value_test/value_data/{architecture}_grad_naive.pt",
-                 f"value_test/value_data/{architecture}_grad_elegant.pt",
-                 verbose=True)
+    # Test grads of Elegant
+    test(model_type, architecture, "elegant", "hooks")
